67058a727e0f5bbd8cbaa9733acda243fad677a8
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-02-24 08:52:24 +0530

    Register page design added

---
+++ client/pages/register.tsx
@@ -0,0 +1,72 @@
+import { FormEvent, useState } from "react";
+
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log({ email, password, confirmPassword });
+  };
+
+  return (
+    <form className="max-w-md mx-auto mt-4" onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 font-bold mb-2"
+          htmlFor="confirmPassword"
+        >
+          Confirm Password
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="confirmPassword"
+          type="password"
+          placeholder="Confirm your password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </div>
+      <div className="flex items-center justify-center">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Sign Up
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default Register;
