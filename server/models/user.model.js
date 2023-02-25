c9233ecb18670db9c186f55100fb21660298ba44
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-02-25 19:18:49 +0530

    user model changed

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,7 +4,6 @@ const User = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    confirmPassword: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
   },
   { collection: "USERS" }
