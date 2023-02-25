4f728c8ce5f1a36a8ac6a48dc49767aa55804bf6
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-02-25 18:37:20 +0530

    user model created

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 
 const User = new mongoose.Schema(
   {
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     confirmPassword: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
   },
-  {
-    collection: "USERS",
-  }
+  { collection: "USERS" }
 );
 
 module.exports = mongoose.model("user", User);
