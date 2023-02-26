40d5e7dc9dc6a6678b16957919fe76617062f5b6
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-02-26 17:01:34 +0530

    User schema updated, role and isVerified added

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -2,11 +2,16 @@ const mongoose = require("mongoose");
 
 const User = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true },
+    email: {
+      type: String,
+      required: [true, "Please enter a email"],
+      unique: true,
+    },
     password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+    isVerified: { type: Boolean, default: false },
   },
-  { collection: "USERS" }
+  { collection: "USERS", timestamps: true }
 );
 
 module.exports = mongoose.model("user", User);
