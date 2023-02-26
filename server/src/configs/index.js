6ad9193461472a463375c917f705c41c9efa4015
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-02-26 09:52:00 +0530

    JWT_SECRET added to .env

--- server/src/configs/index.js
+++ server/src/configs/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const configs = {
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT || 1335,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
 
 module.exports = configs;
