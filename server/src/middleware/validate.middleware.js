67352e96413ff160ea6f0b45f4095a62cf3ddbb5
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-02-27 21:09:17 +0530

    error proper formatting complete from server side

--- server/src/middleware/validate.middleware.js
+++ server/src/middleware/validate.middleware.js
@@ -3,9 +3,11 @@ const { validationResult } = require("express-validator");
 const validate = async (req, res, next) => {
   try {
     // Check for validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty())
-      return res.status(400).json({ msg: errors.array(), isSuccess: false });
+    const errors = validationResult(req).formatWith(({ msg }) => `${msg}`);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.status(422).json({ msg: errors.array(), isSuccess: false });
+    }
     next();
   } catch (error) {
     res
